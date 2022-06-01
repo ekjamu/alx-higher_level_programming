f57edb32728258699dadd717f839d6def60c0efc
ekjamu <ekjamu@gmail.com>
2022-06-01 13:39:55 -0700

    Upload 0 and 1

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+number = string(number)
+
+if (number > 0 ){
+print("%f, is positive",number)
+        }
+else if(number ==0){
+print("%f, is zero",number);
+
+        }
+else{
+print("%f, is negative",number);
+
+        }
