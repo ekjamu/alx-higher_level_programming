0007f72fe27dacdfd9910b92cf3f263cbaeb059e
ekjamu <ekjamu@gmail.com>
2022-06-01 15:23:55 -0700

    Extra Credit

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,5 @@
+for i in range(122,98,-1):
+    if(i%2 ==0):
+        print(chr(i))
+    else:
+        print(chr(i-32))
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,6 @@
+def remove_char_at(str,n):
+
+   a = string[ : n]
+   
+   b = string[n + 1: ]
+   print(a + b)
