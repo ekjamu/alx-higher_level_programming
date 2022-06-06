ef5900e0a90daf325120ac111040d267c6abc409
ekjamu <ekjamu@gmail.com>
2022-06-06 11:45:29 -0700

    Test 3

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(alist):
+def print_list_integer(my_list):
     s = "{integer:d}"
-    for i in alist:
+    for i in my_list:
         print(s.format(integer = i))
